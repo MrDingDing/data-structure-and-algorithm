48f547bf7104f35eafe5e12f183d5b064da8062b
blackjack <2637201260@qq.com>
2019-03-12 17:51:10 +0800

    测试BloackQueue代码

--- src/main/java/com/blackjack/queue/LinkListTest.java
+++
@@ -1,8 +0,0 @@
-package com.blackjack.queue;
-
-/**
- * @author blackjack
- * @date 2019/3/11/17:36
- */
-public class LinkListTest {
-}
---
+++ src/main/java/com/blackjack/set/HashSetTest.java
@@ -0,0 +1,8 @@
+package com.blackjack.set;
+
+/**
+ * @author blackjack
+ * @date 2019/3/11/17:48
+ */
+public class HashSetTest {
+}
